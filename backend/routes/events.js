dfa5419971551106315fcc10e367162bae515ed1
Nancy <nhoushop@outlook.com>
2024-02-09 02:19:26 -0500

    Added methods to routes in vercel.json

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    const events =  getAll();
+    const events = await getAll();
     setTimeout(() => {
       res.json({ events: events });
     }, 2000);
@@ -22,7 +22,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const event =  get(req.params.id);
+    const event = await get(req.params.id);
     res.json({ event: event });
   } catch (error) {
     next(error);
@@ -58,7 +58,7 @@ router.post('/', async (req, res, next) => {
   }
 
   try {
-     add(data);
+    await add(data);
     res.status(201).json({ message: 'Event saved.', event: data });
   } catch (error) {
     next(error);
@@ -94,7 +94,7 @@ router.patch('/:id', async (req, res, next) => {
   }
 
   try {
-     replace(req.params.id, data);
+    await replace(req.params.id, data);
     res.json({ message: 'Event updated.', event: data });
   } catch (error) {
     next(error);
@@ -103,7 +103,7 @@ router.patch('/:id', async (req, res, next) => {
 
 router.delete('/:id', async (req, res, next) => {
   try {
-     remove(req.params.id);
+    await remove(req.params.id);
     res.json({ message: 'Event deleted.' });
   } catch (error) {
     next(error);
