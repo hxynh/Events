5cdfee5892564c2e0fdb9ff52496ed496d302846
Nancy <nhoushop@outlook.com>
2024-02-14 01:04:46 -0500

    Made changes to the path in event.js by importing 'path'

--- backend/data/event.js
+++ backend/data/event.js
@@ -1,16 +1,18 @@
 const fs = require('node:fs/promises');
-
+const path = require('path')
 const { v4: generateId } = require('uuid');
 
 const { NotFoundError } = require('../util/errors');
 
+const filePath = path.join(__dirname, 'events.json');
+
 async function readData() {
-  const data = await fs.readFile('../events.json', 'utf8');
+  const data = await fs.readFile(filePath, 'utf8');
   return JSON.parse(data);
 }
 
 async function writeData(data) {
-  await fs.writeFile('../events.json', JSON.stringify(data));
+  await fs.writeFile(filePath, JSON.stringify(data));
 }
 
 async function getAll() {
